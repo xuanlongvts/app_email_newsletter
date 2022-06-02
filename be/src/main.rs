adc1877886cf6edb5c87b58d202474700310bd31
LongLe <xuanlongvts@gmail.com>
2022-06-02 22:45:41 +0700

    script migrate db

--- be/src/main.rs
+++ be/src/main.rs
@@ -4,6 +4,9 @@ use std::net::TcpListener;
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+
+    println!("port: {}", port);
 
     run(listener)?.await
 }
