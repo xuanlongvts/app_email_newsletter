ca47428767c287edd635447bea3a8b3eba888712
LongLe <xuanlongvts@gmail.com>
2022-06-05 23:32:23 +0700

    two test done

--- be/src/routes/subscriptions.rs
+++ be/src/routes/subscriptions.rs
@@ -1,4 +1,7 @@
 use actix_web::{web, HttpResponse};
+use chrono::Utc;
+use sqlx::PgPool;
+use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -6,6 +9,25 @@ pub struct FormData {
 	name: String,
 }
 
-pub async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
-	HttpResponse::Ok().finish()
+pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+	let id = sqlx::types::Uuid::from_u128(Uuid::new_v4().as_u128());
+	match sqlx::query!(
+		r#"
+		INSERT INTO subscriptions (id, name, email, subscribed_at)
+		VALUES ($1, $2, $3, $4)
+		"#,
+		id,
+		form.email,
+		form.name,
+		Utc::now()
+	)
+	.execute(pool.get_ref())
+	.await
+	{
+		Ok(_) => HttpResponse::Ok().finish(),
+		Err(e) => {
+			println!("Failed to execute query: {}", e);
+			HttpResponse::InternalServerError().finish()
+		}
+	}
 }
