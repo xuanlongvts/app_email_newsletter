7aff6a46d635557c1fe65d0663e7fe261ff116bf
LongLe <xuanlongvts@gmail.com>
2022-06-08 15:58:24 +0700

    telemetry done

---
+++ be/readme.md
@@ -0,0 +1,4 @@
+1. Api curl subscriptions
+   curl -X POST -d 'name=Long Le' -d 'email=longle@gmail.com' http://127.0.0.1:8000/subscriptions
+2. Check packages unsed
+   sudo cargo +nightly udeps
--- be/src/main.rs
+++ be/src/main.rs
@@ -1,10 +1,18 @@
 use email_newsletter::configuration::get_configuration;
 use email_newsletter::startup::run;
+use email_newsletter::telemetry::{get_subscriber, init_subscriber};
+
 use sqlx::PgPool;
 use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    let subscriber = get_subscriber(
+        "app_email_newsletter".into(),
+        "info".into(),
+        std::io::stdout,
+    );
+    init_subscriber(subscriber);
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
