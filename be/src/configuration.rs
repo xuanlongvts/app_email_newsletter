4578d862fcfd1f20d75a99fc8428cb7126a4f0a2
Long Lê <longle@Longs-MacBook-Pro.local>
2022-06-11 00:41:00 +0700

    docker first step deploy success

--- be/src/configuration.rs
+++ be/src/configuration.rs
@@ -4,46 +4,96 @@ use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
 pub struct DatabaseSettings {
-	pub username: String,
-	pub password: Secret<String>,
-	pub port: u16,
-	pub host: String,
-	pub database_name: String,
+    pub username: String,
+    pub password: Secret<String>,
+    pub port: u16,
+    pub host: String,
+    pub database_name: String,
 }
 
 #[derive(Debug, Deserialize)]
 pub struct Settings {
-	pub database: DatabaseSettings,
-	pub application_port: u16,
+    pub database: DatabaseSettings,
+    pub application: ApplicationSettings,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct ApplicationSettings {
+    pub port: u16,
+    pub host: String,
 }
 
 pub fn get_configuration() -> Result<Settings, ConfigError> {
-	let settings = Config::builder()
-		.add_source(File::with_name("configuration"))
-		.build()?;
+    let environment: Environment = std::env::var("APP_ENVIRONMENT")
+        .unwrap_or_else(|_| "local".into())
+        .try_into()
+        .expect("Failed to parse APP_ENVIRONMENT.");
 
-	settings.try_deserialize()
+    let base_path = std::env::current_dir().expect("Failed to determine the current directory");
+    let configuration_directory = base_path.join("configuration");
+
+    let base_path = configuration_directory.join("base");
+    let curr_env_path = configuration_directory.join(environment.as_str());
+
+    let settings = Config::builder()
+        .add_source(File::from(base_path).required(true))
+        .add_source(File::from(curr_env_path).required(true))
+        // Add in settings from environment variables (with a prefix of APP and '__' as separator)
+        // E.g. `APP_APPLICATION__PORT=5001 would set `Settings.application.port`
+        .add_source(config::Environment::with_prefix("app").separator("__"))
+        .build()?;
+
+    settings.try_deserialize()
 }
 
 impl DatabaseSettings {
-	pub fn connection_string(&self) -> Secret<String> {
-		Secret::new(format!(
-			"postgres://{}:{}@{}:{}/{}",
-			self.username,
-			self.password.expose_secret(),
-			self.host,
-			self.port,
-			self.database_name
-		))
-	}
+    pub fn connection_string(&self) -> Secret<String> {
+        Secret::new(format!(
+            "postgres://{}:{}@{}:{}/{}",
+            self.username,
+            self.password.expose_secret(),
+            self.host,
+            self.port,
+            self.database_name
+        ))
+    }
 
-	pub fn connection_string_without_db(&self) -> Secret<String> {
-		Secret::new(format!(
-			"postgres://{}:{}@{}:{}",
-			self.username,
-			self.password.expose_secret(),
-			self.host,
-			self.port
-		))
-	}
+    pub fn connection_string_without_db(&self) -> Secret<String> {
+        Secret::new(format!(
+            "postgres://{}:{}@{}:{}",
+            self.username,
+            self.password.expose_secret(),
+            self.host,
+            self.port
+        ))
+    }
+}
+
+pub enum Environment {
+    Local,
+    Production,
+}
+
+impl Environment {
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            Environment::Local => "local",
+            Environment::Production => "production",
+        }
+    }
+}
+
+impl TryFrom<String> for Environment {
+    type Error = String;
+
+    fn try_from(s: String) -> Result<Self, Self::Error> {
+        match s.to_lowercase().as_str() {
+            "local" => Ok(Self::Local),
+            "production" => Ok(Self::Production),
+            other => Err(format!(
+                "{} is not a supported environment. Use either `local` or `production`.",
+                other
+            )),
+        }
+    }
 }
--- be/src/main.rs
+++ be/src/main.rs
@@ -1,9 +1,11 @@
+use std::net::TcpListener;
+
+use secrecy::ExposeSecret;
+use sqlx::PgPool;
+
 use email_newsletter::configuration::get_configuration;
 use email_newsletter::startup::run;
 use email_newsletter::telemetry::{get_subscriber, init_subscriber};
-use secrecy::ExposeSecret;
-use sqlx::PgPool;
-use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
@@ -15,10 +17,9 @@ async fn main() -> std::io::Result<()> {
     init_subscriber(subscriber);
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool =
-        PgPool::connect(&configuration.database.connection_string().expose_secret())
-            .await
-            .expect("Failed to connect to Postgres.");
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+        PgPool::connect_lazy(&configuration.database.connection_string().expose_secret()).expect("Failed to connect to Postgres.");
+
+    let address = format!("{}:{}", configuration.application.host, configuration.application.port);
     let listener = TcpListener::bind(address)?;
 
     run(listener, connection_pool)?.await
