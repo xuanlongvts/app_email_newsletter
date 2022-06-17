365c102a74e04473641a33c1c653679a5cccb65e
Long Le <xuanlongvts@gmail.com>
2022-06-17 17:48:30 +0700

    refactor lifttime

--- be/src/email_client.rs
+++ be/src/email_client.rs
@@ -13,12 +13,13 @@ pub struct EmailClient {
 }
 
 #[derive(Serialize)]
-struct SendEmailRequest {
-    from: String,
-    to: String,
-    subject: String,
-    html_body: String,
-    text_body: String,
+#[serde(rename_all = "PascalCase")]
+struct SendEmailRequest<'a> {
+    from: &'a str,
+    to: &'a str,
+    subject: &'a str,
+    html_body: &'a str,
+    text_body: &'a str,
 }
 
 impl EmailClient {
@@ -31,15 +32,15 @@ impl EmailClient {
         }
     }
 
-    pub async fn send_email(&self, recipient: SubscriberEmail, subject: &str, html_content: &str, text_content: &str) -> Result<(), reqwest::Error> {
+    pub async fn send_email(&self, recipient: SubscriberEmail, subject: &str, html_body: &str, text_body: &str) -> Result<(), reqwest::Error> {
         let url = format!("{}/email", self.base_url);
 
         let request_body = SendEmailRequest {
-            from: self.sender.as_ref().to_owned(),
-            to: recipient.as_ref().to_owned(),
-            subject: subject.to_owned(),
-            html_body: html_content.to_owned(),
-            text_body: text_content.to_owned(),
+            from: self.sender.as_ref(),
+            to: recipient.as_ref(),
+            subject,
+            html_body,
+            text_body,
         };
 
         let _builder = self.http_client.post(&url)
@@ -59,12 +60,27 @@ mod tests {
     use fake::faker::internet::en::SafeEmail;
     use fake::faker::lorem::en::{Paragraph, Sentence};
     use fake::{Fake, Faker};
+    use wiremock::Request;
     use wiremock::matchers::{header, header_exists, path, method};
     use wiremock::{Mock, MockServer, ResponseTemplate};
     use secrecy::Secret;
 
+    struct SendEmailBodyMatcher;
+
+    impl wiremock::Match for SendEmailBodyMatcher {
+        fn matches(&self, request: &Request) -> bool {
+            let result: Result<serde_json::Value, _> = serde_json::from_slice(&request.body);
+            if let Ok(body) = result {
+                // dbg!(&body);
+                body.get("From").is_some() && body.get("To").is_some() && body.get("Subject").is_some() && body.get("HtmlBody").is_some() && body.get("TextBody").is_some()
+            } else {
+                false
+            }
+        }
+    }
+
     #[tokio::test]
-    async fn send_email_fires_a_request_to_base_url() {
+    async fn send_email_sends_the_expected_request() {
         // Arrange
         let mock_server = MockServer::start().await;
         let sender = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
@@ -74,6 +90,7 @@ mod tests {
             .and(header("Content-Type", "application/json"))
             .and(path("/email"))
             .and(method("POST"))
+            .and(SendEmailBodyMatcher)
             .respond_with(ResponseTemplate::new(200))
             .expect(1)
             .mount(&mock_server)
