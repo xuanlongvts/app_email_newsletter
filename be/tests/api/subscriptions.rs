33634018c81d40fdcd5c9a064f6182155f68374a
Long Le <xuanlongvts@gmail.com>
2022-06-23 22:08:32 +0700

    refactor subscription email send a link

--- be/tests/api/subscriptions.rs
+++ be/tests/api/subscriptions.rs
@@ -1,11 +1,12 @@
 use crate::helpers::spawn_app;
+use linkify::{LinkFinder, LinkKind};
 use wiremock::matchers::{method, path};
 use wiremock::{Mock, ResponseTemplate};
 
 #[tokio::test]
 async fn subscribe_returns_a_200_for_valid_form_data() {
     let app = spawn_app().await;
-    let body = "name=Long%20Le&email=xuanlongvts%40gmail.com";
+    let body = "name=Long%20Le&email=longle%40gmail.com";
 
     Mock::given(path("/email"))
         .and(method("POST"))
@@ -21,9 +22,9 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
         .fetch_one(&app.db_pool)
         .await
         .expect("Failed to fetch saved subscription.");
-    println!("saved: {:?}", saved);
+    // println!("saved: {:?}", saved);
     assert_eq!(saved.name, "Long Le");
-    assert_eq!(saved.email, "xuanlongvts@gmail.com");
+    assert_eq!(saved.email, "longle@gmail.com");
 }
 
 #[tokio::test]
@@ -84,4 +85,38 @@ async fn subscribe_sends_a_confirmation_email_for_valid_data() {
 
     // Act
     app.post_subscriptions(body.into()).await;
+}
+
+#[tokio::test]
+async fn subscribe_sends_a_confirmation_email_with_a_link() {
+    // Arrange
+    let app = spawn_app().await;
+    let body = "name=Long%20Le&email=longle%40gmail.com";
+
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .expect(1)
+        .mount(&app.email_server)
+        .await;
+
+    app.post_subscriptions(body.into()).await;
+
+    let email_request = &app.email_server.received_requests().await.unwrap()[0];
+    let body: serde_json::Value = serde_json::from_slice(&email_request.body).unwrap();
+
+    // Extract the link from one of the request fields.
+    let get_link = |s: &str| {
+        let links: Vec<_> = LinkFinder::new()
+            .links(s)
+            .filter(|l| *l.kind() == LinkKind::Url)
+            .collect();
+        assert_eq!(links.len(), 1);
+        links[0].as_str().to_owned()
+    };
+    let html_link = get_link(&body["HtmlBody"].as_str().unwrap());
+    let text_link = get_link(&body["TextBody"].as_str().unwrap());
+
+    // The two links should be identical
+    assert_eq!(html_link, text_link);
 }
