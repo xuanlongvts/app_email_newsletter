563b1f698cbdeffe1032cc4ca5b09d9bb3769eb0
Long Le <xuanlongvts@gmail.com>
2022-06-24 15:32:46 +0700

    green test get subscription/confirm

--- be/src/routes/subscriptions.rs
+++ be/src/routes/subscriptions.rs
@@ -88,7 +88,7 @@ pub async fn insert_subscriber(
     sqlx::query!(
         r#"
 		INSERT INTO subscriptions (id, email, name, subscribed_at, status)
-		VALUES ($1, $2, $3, $4, 'confirmed')
+		VALUES ($1, $2, $3, $4, 'pending_confirmation')
 		"#,
         id,
         new_subscriber.email.as_ref(),
--- be/tests/api/subscriptions.rs
+++ be/tests/api/subscriptions.rs
@@ -17,14 +17,31 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
     // Act
     let response = app.post_subscriptions(body.into()).await;
     assert_eq!(200, response.status().as_u16());
+}
 
-    let saved = sqlx::query!("SELECT name, email FROM subscriptions",)
+#[tokio::test]
+async fn subscribe_persists_the_new_subscriber() {
+    // Arrange
+    let app = spawn_app().await;
+    let body = "name=Long%20Le&email=longle%40gmail.com";
+
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .mount(&app.email_server)
+        .await;
+
+    // Act
+    app.post_subscriptions(body.into()).await;
+
+    // Assert
+    let saved = sqlx::query!("SELECT name, email, status FROM subscriptions",)
         .fetch_one(&app.db_pool)
         .await
         .expect("Failed to fetch saved subscription.");
-    // println!("saved: {:?}", saved);
     assert_eq!(saved.name, "Long Le");
     assert_eq!(saved.email, "longle@gmail.com");
+    assert_eq!(saved.status, "pending_confirmation");
 }
 
 #[tokio::test]
